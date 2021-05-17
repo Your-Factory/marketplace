deeff29e269eb728f814570eed26c353489ef60a
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2021-05-17 20:48:47 +0500

    Add login page template

--- frontend/app.py
+++ frontend/app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def main_page():
     return render_template('store_front.html')
+
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
