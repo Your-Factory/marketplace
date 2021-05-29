bceb0f35245cf9dfef8e12551ae25659e432d485
Mikhail Elenskii <elenskii-mikhail@outlook.com>
2021-05-29 22:59:05 +0500

    Use boostrap from CDN

--- static/js/preview.js
+++ static/js/preview.js
@@ -26,7 +26,7 @@ function init(container) {
 
     scene = new THREE.Scene();
 
-    const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+    const ambientLight = new THREE.AmbientLight(0x9ab4ff, 0.4);
     scene.add(ambientLight);
 
     const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
